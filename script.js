1cdbee38c420081137b604a1c9578d71e0eea0d2
Gaurav Nagar <gauravnagar365@gmail.com>
2024-12-28 12:15:58 +0530

    add style to our project

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let url='https://catfact.ninja/fact';
-
+let btn=document.getElementById('btn');
+let cont=document.getElementById('cont');
 async function getthequotes() {
     let response=await fetch(url);
-    let data=await response.json()
-    console.log(data)
+    let data=await response.json();
+    cont.innerHTML=`
+    <h3>FACT</h3>
+    <p>${data.fact}</p>`
+    cont.style.display='flex'
 }
-getthequotes();
+
+btn.onclick=getthequotes;
